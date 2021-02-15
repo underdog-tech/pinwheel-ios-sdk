b62642b575fcbba1b400083c2c66c88d14b9be84
Robby Abaya <rabaya@gmail.com>
2021-02-15 07:29:01 -0500

    Consolidate amount payload models

--- Events/PinwheelInputAmountEvent.swift
+++ Events/PinwheelInputAmountEvent.swift
@@ -10,5 +10,5 @@ import Foundation
 public struct PinwheelInputAmountEvent: Codable {
     public var type: String
     public var eventName: String
-    public var payload: PinwheelInputAmountPayload
+    public var payload: PinwheelAmountPayload
 }
