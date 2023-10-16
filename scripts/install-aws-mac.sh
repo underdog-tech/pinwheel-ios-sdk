54b65966883bea731ac06538090b6498bcdd8c18
Rene Martin Loperena Garcia <rene@pinwheelapi.com>
2023-10-16 17:21:09 -0600

    Add aws install

---
+++ scripts/install-aws-mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
+set -e
+
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+
+sudo installer -pkg AWSCLIV2.pkg -target /
+
+echo \>\> Location is $(which aws)
+echo \>\> Version is $(aws --version)
