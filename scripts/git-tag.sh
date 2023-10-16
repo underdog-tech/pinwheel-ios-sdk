434c64902198eff8d2c9b0bfb761921f8958ed08
Rene Martin Loperena Garcia <rene@pinwheelapi.com>
2023-10-16 17:55:08 -0600

    Fix git-tag

--- scripts/git-tag.sh
+++ scripts/git-tag.sh
@@ -5,5 +5,8 @@ echo $VERSION
 # Tag the commit
 git tag "$VERSION"
 
-# Push the changes to the remote
-git push origin "$VERSION"
+# Add the remote with the access token
+git remote add authenticated https://pinwheel-it-svc:${GITHUB_ACCESS_TOKEN}@github.com/underdog-tech/pinwheel-ios-sdk.git
+
+# Push the tag
+git push authenticated "$VERSION"
