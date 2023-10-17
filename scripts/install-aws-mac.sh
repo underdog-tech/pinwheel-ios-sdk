587e71d4b60db5643abf7876565d65b801fa684e
Rene Loperena <rene@pinwheelapi.com>
2023-10-17 15:11:06 -0600

    Fix aws-cli install

--- scripts/install-aws-mac.sh
+++ scripts/install-aws-mac.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+PACKAGE_FILENAME="AWSCLIV2.pkg"
 # This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
 set -e
 
-curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o $PACKAGE_FILENAME
 
-sudo installer -pkg AWSCLIV2.pkg -target /
+sudo installer -pkg $PACKAGE_FILENAME -target /
 
-rm AWSCLI2.pkg
+# Cleanup
+rm $PACKAGE_FILENAME
 
 echo \>\> Location is $(which aws)
 echo \>\> Version is $(aws --version)
