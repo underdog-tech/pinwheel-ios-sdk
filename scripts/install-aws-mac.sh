314ff31262faa1849bea3f0fe0f8d45e3c004431
Rene Loperena <rene@pinwheelapi.com>
2023-10-17 14:47:40 -0600

    Update PR / aws-cli installation in CI

--- scripts/install-aws-mac.sh
+++ scripts/install-aws-mac.sh
@@ -7,5 +7,7 @@ curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 
 sudo installer -pkg AWSCLIV2.pkg -target /
 
+rm AWSCLI2.pkg
+
 echo \>\> Location is $(which aws)
 echo \>\> Version is $(aws --version)
