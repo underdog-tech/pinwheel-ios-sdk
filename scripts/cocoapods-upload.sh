864c172324ccfe188e8ba89a6b9545a42a504a51
roscioli <octavio@getpinwheel.com>
2023-10-17 12:09:32 -0400

    no reg

--- scripts/cocoapods-upload.sh
+++ scripts/cocoapods-upload.sh
@@ -24,10 +24,12 @@ echo "login $COCOAPODS_TRUNK_EMAIL" >> ~/.netrc
 echo "password $COCOAPODS_TRUNK_TOKEN" >> ~/.netrc
 chmod 0600 /Users/distiller/.netrc
 
-echo \>\> Pod register
+# echo \>\> Pod register
 CURRENT_GIT_TAG=$(git describe --tags --abbrev=0)
 DESCRIPTION="Pushing pinwheel-ios-sdk version $CURRENT_GIT_TAG to Cocoapods"
-pod trunk register $COCOAPODS_TRUNK_EMAIL Octavio --description=$CURRENT_GIT_TAG
+# pod trunk register $COCOAPODS_TRUNK_EMAIL Octavio --description=$CURRENT_GIT_TAG
+
+echo \>\> $DESCRIPTION
 
 echo \>\> Pod me
 pod trunk me
