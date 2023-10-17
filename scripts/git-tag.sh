ee050f5b9e1f4bcaa5307698c3a5d66c34a41fb3
roscioli <octavio@getpinwheel.com>
2023-10-17 09:05:57 -0400

    pf

--- scripts/git-tag.sh
+++ scripts/git-tag.sh
@@ -10,12 +10,8 @@ echo $VERSION
 # Add the remote with the access token
 git remote add authenticated https://pinwheel-it-svc:${GITHUB_ACCESS_TOKEN}@github.com/underdog-tech/pinwheel-ios-sdk.git
 
-# Delete the old tag
-git pull authenticated --tags
-# git tag -d "$VERSION"
-
 # Add new tag
 git tag "$VERSION"
 
 # Push the tag
-git push authenticated --tags
+git push -f authenticated --tags
