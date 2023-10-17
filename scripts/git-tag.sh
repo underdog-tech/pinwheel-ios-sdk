d9ed923142cc8c67a6e169900d152462b98b2f6a
roscioli <octavio@getpinwheel.com>
2023-10-17 09:03:50 -0400

    4

--- scripts/git-tag.sh
+++ scripts/git-tag.sh
@@ -11,7 +11,7 @@ echo $VERSION
 git remote add authenticated https://pinwheel-it-svc:${GITHUB_ACCESS_TOKEN}@github.com/underdog-tech/pinwheel-ios-sdk.git
 
 # Delete the old tag
-git pull --tags
+git pull authenticated --tags
 # git tag -d "$VERSION"
 
 # Add new tag
